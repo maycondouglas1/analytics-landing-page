e824408f712926ea9a27644da82bf138b34ad391
Maycon Douglas <mayconmrqs2001@gmail.com>
2023-01-24 21:09:27 -0300

    Implemented Newsletter container

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Analytics from "./containers/Analytics";
 import Hero from "./containers/Hero";
+import Newsletter from "./containers/Newsletter";
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
       <Navbar /> 
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }
